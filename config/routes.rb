a6bcb3d168c724b7388b5535f7c5f00740688f32
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-04 23:47:35 -0500

    created database, and first oil: backend development

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :oils do
-  get '/', to: 'oils#index'
+  get "/", to: 'oils#index'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")

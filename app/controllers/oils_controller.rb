c46161533974e5c2de08c0c8010206842df090df
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-17 12:01:45 -0500

    adding CRUD to backend

--- app/controllers/oils_controller.rb
+++ app/controllers/oils_controller.rb
@@ -5,8 +5,18 @@ class OilsController < ApplicationController
         if oils
             render json: oils, status: :ok
         else
-            render json: { error: "error, no oils to show"}, status: :not_found
+            render json: { error: ["Error, no oils to show"]}, status: :not_found
         end
     end
+
+    def show 
+        oil = find_oil
+        if oil
+            render json: oil, status: :ok
+        else
+            render json {error: ["No oil to show"]}
+        end
+    end
+
 
 end

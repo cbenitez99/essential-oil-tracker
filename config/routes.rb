9dc76aecf472105bb400684dbfcae3e7e0c17234
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-17 18:06:42 -0500

    added routes to rails backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'oils#index'
 
-  resources :oils
+  resources :oils, only: [:index, :show]
+  resources :users, only: [:show, :create]
+
   post "/login", to: "sessions#create"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

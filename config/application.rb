fddafac364c20fff6bef640dbd02d965f26e8ca1
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-28 17:30:52 -0500

    added middleware in application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,16 @@ module EssentialOilTracker
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.action_dispatch.cookies_same_site_protection = :strict
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+
   end
 end

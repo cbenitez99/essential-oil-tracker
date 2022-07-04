40b93a7e67bf42b9c15aec1dec65a979d2c1186b
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-04 13:36:55 -0500

    tweaked user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :oils
-    validates :username, presence:true, length:{within:6..32} , format: {with: /^[a-zA-Z0-9_ ]*$/, message:"can contain alphabets, numbers and underscores only"}    
-    validates :password, presence:true, length:{within:8..32}, confirmation:true
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
 end

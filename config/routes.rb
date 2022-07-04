9ea244f7bbb7c0474dd442a7b5bf4cb816d2ae9f
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-04 14:10:55 -0500

    added /logout routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-
   root 'oils#index'
-
   resources :oils, only: [:index, :show]
   resources :users, only: [:show, :create]
-
   post "/login", to: "sessions#create"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  delete "/logout", to: "sessions#destroy"
 end

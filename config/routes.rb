921719e2310a3cc3a81caefd973f8ac6550838ac
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-10 21:14:59 -0500

    added serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'oils#index'
-  resources :oils, only: [:index, :show, :create]
+  resources :oils, only: [:index, :show, :create, :destroy]
   resources :users, only: [:show, :create]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"

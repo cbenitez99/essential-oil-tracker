3350122993087b356f3c35af45daa5dea847f7fa
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-23 16:28:33 -0500

    user_oils: success?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :oils
+    has_many :user_oils
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
 end

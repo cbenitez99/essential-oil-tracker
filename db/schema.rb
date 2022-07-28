ce3c0ddbdb2e3ab692cae642329d80209a0da6bf
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-28 13:43:25 -0500

    changed schema to include image property on Products

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_022650) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.integer "amount"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

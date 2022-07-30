735e9287d7e076c59e7953156a17e5ec9637d189
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-30 02:07:27 -0500

    added Patch route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index, :show, :create]
-  # root 'oils#index'
-  resources :user_oils, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :show, :create]
+  resources :products, only: [:index, :show, :create]
+  resources :user_oils, only: [:index, :show, :create, :destroy]
+
+  patch "/user_oils/:id", to: "user_oils#update"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end

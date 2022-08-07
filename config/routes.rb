fa5f241fe673ea09748a2e4156750f95b89ad691
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-08-06 22:39:12 -0500

    can add oils: success

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create]
   resources :products, only: [:index, :show, :create]
-  resources :user_oils, only: [:index, :show, :create, :destroy]
+  resources :user_oils, only: [:index, :show, :create, :update, :destroy]
 
-  patch "/user_oils/:id", to: "user_oils#update"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end

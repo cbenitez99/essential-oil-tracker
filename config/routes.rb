e72c6e268a35b2e87c8b9426b8d66bc7dc1bf215
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-08-06 23:32:45 -0500

    routes correct, keep

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] #user view for debugging ONLY
   resources :products, only: [:index, :show, :create]
-  resources :user_oils, only: [:index, :show, :create, :update, :destroy]
+  resources :user_oils, only: [:index, :show, :create, :destroy]
 
+  patch "/user_oils/:id", to: "user_oils#update"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
